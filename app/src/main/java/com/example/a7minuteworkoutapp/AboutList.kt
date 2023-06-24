8f3ec6b8ff38a8cfeae268993b0fd5962885d2bb
Samarth-Natrajan <samarthshridharnatrajan.bt21ece@pec.edu.in>
2023-06-24 11:58:55 +0530

    exercise added to recycler view...left with their detailed explanation

--- app/src/main/java/com/example/a7minuteworkoutapp/AboutList.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/AboutList.kt
@@ -6,6 +6,13 @@ object AboutList {
         AboutExercise("Wall Sit",R.drawable.ic_wall_sit,"ergnoringrngti"),
         AboutExercise("Push Up",R.drawable.ic_push_up,"smkjgmltuhvm"),
         AboutExercise("Abdominal Crunch",R.drawable.ic_abdominal_crunch,"ihgeigoetjg"),
-        AboutExercise("Abdominal Crunch",R.drawable.ic_abdominal_crunch,"ihgeigoetjg")
-    )
+        AboutExercise("Step up onto Chair",R.drawable.ic_step_up_onto_chair,"ihgeigoetjgjn"),
+        AboutExercise("Squat",R.drawable.ic_squat,"ejktngtjn"),
+        AboutExercise("Tricep Dip on Chair",R.drawable.ic_triceps_dip_on_chair,"ekjrngerlgbjg"),
+        AboutExercise("Plank",R.drawable.ic_plank,"oioenfrjge"),
+        AboutExercise("High Knees",R.drawable.ic_high_knees_running_in_place,"p2ofnringtng"),
+        AboutExercise("Lunges",R.drawable.ic_lunge,"wrgorgbtrj"),
+        AboutExercise("Push-up & Rotation",R.drawable.ic_push_up_and_rotation,"o3gb3i5brgi"),
+        AboutExercise("Side Plank",R.drawable.ic_side_plank,"roing3itbgit")
+        )
 }
