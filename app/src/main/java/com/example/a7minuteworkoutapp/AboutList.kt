b9baf89659ec7628f6f027e7a83d4f4cc1658087
Samarth-Natrajan <samarthshridharnatrajan.bt21ece@pec.edu.in>
2023-06-24 17:09:31 +0530

    complete

--- app/src/main/java/com/example/a7minuteworkoutapp/AboutList.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/AboutList.kt
@@ -2,7 +2,7 @@ package com.example.a7minuteworkoutapp
 
 object AboutList {
     val aboutList = listOf<AboutExercise>(
-        AboutExercise("Jumping-Jacks",R.drawable.ic_jumping_jacks,"1. Stand upright with your legs together, arms at your sides.\n" +
+        AboutExercise("1. Jumping-Jacks",R.drawable.ic_jumping_jacks,"1. Stand upright with your legs together, arms at your sides.\n" +
                 "\n" +
                 "2. Bend your knees slightly, and jump into the air. \n" +
                 "\n" +
@@ -11,7 +11,7 @@ object AboutList {
                 "4. Jump back to starting position.\n" +
                 "\n" +
                 "5. Repeat."),
-        AboutExercise("Wall Sit",R.drawable.ic_wall_sit,"1. stand with your back flat against a wall.\n" +
+        AboutExercise("2. Wall Sit",R.drawable.ic_wall_sit,"1. stand with your back flat against a wall.\n" +
             "\n"+
                 "2. Walk your feet out about about 2 feet in front of you, feet spread about 6 inches apart.\n" +
                 "\n" +
@@ -21,7 +21,7 @@ object AboutList {
                 "\n" +
                 "5. Straighten your legs and come back to a standing lean against the wall."
         ),
-        AboutExercise("Push Up",R.drawable.ic_push_up,"1. Get down on all fours, placing your hands slightly wider than your shoulders. \n" +
+        AboutExercise("3. Push Up",R.drawable.ic_push_up,"1. Get down on all fours, placing your hands slightly wider than your shoulders. \n" +
                 "\n" +
                 "2. Straighten your arms and legs. \n" +
                 "\n" +
@@ -30,7 +30,7 @@ object AboutList {
                 "4. Pause, then push yourself back up. \n" +
                 "\n" +
                 "5. Repeat."),
-        AboutExercise("Abdominal Crunch",R.drawable.ic_abdominal_crunch,"1. Keep your knees comfortably apart.\n" +
+        AboutExercise("4. Abdominal Crunch",R.drawable.ic_abdominal_crunch,"1. Keep your knees comfortably apart.\n" +
                 "\n" +
                 "2. Fold your arms on your chest and tighten your abdominal muscles.\n" +
                 "\n"+
@@ -39,10 +39,10 @@ object AboutList {
                 "4. Hold for three deep breaths, then return to starting position.\n" +
                 "\n"+
                 "5. Repeat" ),
-        AboutExercise("Step up onto Chair",R.drawable.ic_step_up_onto_chair,"1.Walkup to the chair with chair-sit facing in front of you.\n" +
+        AboutExercise("5. Step up onto Chair",R.drawable.ic_step_up_onto_chair,"1.Walkup to the chair with chair-sit facing in front of you.\n" +
                 "\n" +
                 "2. You should be 2 feet away from the chair. Next, take your right foot, step up and continue looking all the straight ahead and then stay back down. Similarly, switch legs, step back up and come down."),
-        AboutExercise("Squat",R.drawable.ic_squat,"1. Stand straight with feet hip-width apart.\n" + "\n"+
+        AboutExercise("6. Squat",R.drawable.ic_squat,"1. Stand straight with feet hip-width apart.\n" + "\n"+
                 "2. Tighten your stomach muscles.\n" +
                 "\n"+
                 "3. Lower down, as if sitting in an invisible chair.\n" +
@@ -50,7 +50,7 @@ object AboutList {
                 "4. Straighten your legs to lift back up.\n" +
                 "\n"+
                 "5. Repeat the movement"),
-        AboutExercise("Tricep Dip on Chair",R.drawable.ic_triceps_dip_on_chair,"1. Sit on your chair or bench with your arms at your side and your feet flat on the floor, hip distance apart.\n" +
+        AboutExercise("7. Tricep Dip on Chair",R.drawable.ic_triceps_dip_on_chair,"1. Sit on your chair or bench with your arms at your side and your feet flat on the floor, hip distance apart.\n" +
                 "\n" +
                 "2. Position your hands so that your palms are down beside your hips. Your fingers should grip the front of the chair seat.\n" +
                 "\n" +
@@ -59,12 +59,12 @@ object AboutList {
                 "4. Breathe in as you slowly lower your body, hinging at the elbows until each forms a 90-degree angle.\n" +
                 "\n" +
                 "5. Breathe out as you push up to your starting position with your arms fully extended."),
-        AboutExercise("Plank",R.drawable.ic_plank,"1. Begin in the plank position, face down with your forearms and toes on the floor. ...\n" +
+        AboutExercise("8. Plank",R.drawable.ic_plank,"1. Begin in the plank position, face down with your forearms and toes on the floor. ...\n" +
                 "\n"+
                 "2. Engage your abdominal muscles, drawing your navel toward your spine. ...\n" +
                 "\n"+
                 "3.Hold this position for 30 seconds."),
-        AboutExercise("High Knees",R.drawable.ic_high_knees_running_in_place,"1. Stand tall with your feet about hip-to-shoulder-width apart and your arms at your sides.\n" +
+        AboutExercise("9. High Knees",R.drawable.ic_high_knees_running_in_place,"1. Stand tall with your feet about hip-to-shoulder-width apart and your arms at your sides.\n" +
                 "\n" +
                 "2. Looking straight ahead, open your chest, and engage your core muscles.\n" +
                 "\n" +
@@ -73,7 +73,7 @@ object AboutList {
                 "4. Quickly lower your right leg and left hand.\n" +
                 "\n"+
                 "5. Repeat."),
-        AboutExercise("Lunges",R.drawable.ic_lunge,"1. Stand tall with feet hip-width apart. Engage your core.\n" +
+        AboutExercise("10. Lunges",R.drawable.ic_lunge,"1. Stand tall with feet hip-width apart. Engage your core.\n" +
                 "\n" +
                 "2. Take a big step forward with right leg. Start to shift your weight forward so heel hits the floor first.\n" +
                 "\n" +
@@ -83,14 +83,14 @@ object AboutList {
                 "\n" +
                 "5. Repeat on the other side.\n" +
                 "\n"),
-        AboutExercise("Push-up & Rotation",R.drawable.ic_push_up_and_rotation,"1. Start in a standard push-up position, with the weight on the hands and toes.\n" +
+        AboutExercise("11. Push-up & Rotation",R.drawable.ic_push_up_and_rotation,"1. Start in a standard push-up position, with the weight on the hands and toes.\n" +
                 "\n" +
                 "2. Bend the arms to lower the chest to the floor as per a standard push-up.\n" +
                 "\n"+
                 "3. As you push back up, shift your weight onto one side.\n" +
                 "\n"+
                 "4. Rotate your upper body and extend your arm straight up toward the ceiling."),
-        AboutExercise("Side Plank",R.drawable.ic_side_plank,"1. Lie on your right side with your legs straight and feet stacked on top of each other. ...\n" +
+        AboutExercise("12. Side Plank",R.drawable.ic_side_plank,"1. Lie on your right side with your legs straight and feet stacked on top of each other. ...\n" +
                 "\n" +
                 "2. With your neck neutral, breathe out and brace your core.\n" +
                 "\n"+
